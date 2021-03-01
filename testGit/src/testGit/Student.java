dd82ada01d11010b1051e5c98b6d1953d51aa420
LR <LR@DESKTOP-7EPBGC2>
2021-03-01 11:49:32 +0100

    send test

---
+++ testGit/src/testGit/Student.java
@@ -0,0 +1,41 @@
+package testGit;
+
+public class Student {
+	
+	private String name;
+	private int id;
+	
+	
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public Student(String name, int id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
